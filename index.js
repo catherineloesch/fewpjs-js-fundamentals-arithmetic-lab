30b6e29727ff4007eddc1832b431fc867f1a78ee
Catherine Loesch <catherine.loesch@protonmail.com>
2021-05-03 04:43:07 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const num3 = 32;
+const num4 = 7;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()*100)+1;
+const mod = num3 % num4;
+const max = Math.max(2, 7, 13, 14, 17, 18, 20);
